a5aac590aadddc2dcee1664e8a2d995c47c84e6a
Gabrielle Oliveira Santana <gaboliveirasantana1@gmail.com>
2026-08-10 22:06:04 -0300

    feat: add Angular developer agent skills documentation and configuration files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { provideHttpClient } from "@angular/common/http";
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 
 import { routes } from "./app.routes";
 
